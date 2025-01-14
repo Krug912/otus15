6afc19807344399433c8ac33295ba1785cffb59c
Krug912 <162484306+Krug912@users.noreply.github.com>
2025-01-14 19:55:45 +0700

    Update skript_io_test.sh

--- skript_io_test.sh
+++ skript_io_test.sh
@@ -11,11 +11,11 @@ run_process() {
 # Команды для выполнения(например, запись больших файлов)
 command1="dd if=/dev/zero of=file1 bs=1M count=3500"
 command2="dd if=/dev/zero of=file2 bs=1M count=3500"
-
+command3="dd if=/dev/zero of=file2 bs=1M count=3500"
 # Запускаем два процесса с разными уровнями приоритета ionice
-run_process 1 "$command1" &  # Нормальный приоритет
-run_process 2 "$command2" &  # Низкий приоритет
-
+run_process 1 "$command1" &  # Класс планирования Real time
+run_process 2 "$command2" &  # Класс планирования Best effort
+run_process 3 "$command3" &  # Класс планирования Idle
 # Ждем завершения обоих процессов
 wait
 
